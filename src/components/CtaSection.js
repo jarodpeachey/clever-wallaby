0b7945628e58acf5da30cd030327f4e2b4cf11a3
Jarod Peachey <jarodpeachey@gmail.com>
2020-11-07 10:34:23 -0500

    Update CtaSection.js

--- src/components/CtaSection.js
+++ src/components/CtaSection.js
@@ -10,7 +10,7 @@ export default class CtaSection extends React.Component {
         return (
             <section className="section section--cta">
               <div className="container container--lg">
-                <div className={classNames('section__body', 'align-center', {'inverse bg-blue': _.get(section, 'has_background', null) && (_.get(section, 'background_color', null) === 'blue'), 'bg-gray': _.get(section, 'has_background', null) && (_.get(section, 'background_color', null) === 'gray')})}>
+                <div className={classNames('section__body', 'align-center', {'inverse bg-blue': _.get(section, 'has_background', null) && (_.get(section, 'background_color', null) === 'blue'), 'bg-gray': _.get(section, 'has_background', null) && (_.get(section, 'background_color', null) === 'gray'), 'bg-red': _.get(section, 'has_background', null) && (_.get(section, 'background_color', null) === 'red')})}>
                   <div className="container container--md">
                     {_.get(section, 'title', null) && (
                     <h2 className="section__title">{_.get(section, 'title', null)}</h2>
